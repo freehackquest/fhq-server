55948e8430b755255c79d35744ac6de708f02768
Evgenii Sopov <mrseakg@gmail.com>
2017-10-25 18:51:38 +0700

    Implemneted game delete

--- src/cmd/sources/cmd_game_delete_handler.cpp
+++ src/cmd/sources/cmd_game_delete_handler.cpp
@@ -5,17 +5,8 @@
 #include <QCryptographicHash>
 
 CmdGameDeleteHandler::CmdGameDeleteHandler(){
-    m_vInputs.push_back(CmdInputDef("uuid").uuid_().required().description("Global Identificator of the Game"));
-    m_vInputs.push_back(CmdInputDef("logo").string_().required().description("Logo"));
-    m_vInputs.push_back(CmdInputDef("name").string_().required().description("Name of the Game"));
-    m_vInputs.push_back(CmdInputDef("description").string_().required().description("Description of the Game"));
-    m_vInputs.push_back(CmdInputDef("state").string_().required().description("State of the game"));
-    m_vInputs.push_back(CmdInputDef("form").string_().required().description("Form of the game"));
-    m_vInputs.push_back(CmdInputDef("type").string_().required().description("Type of the game"));
-    m_vInputs.push_back(CmdInputDef("date_start").string_().required().description("Date start"));
-    m_vInputs.push_back(CmdInputDef("date_stop").string_().required().description("Date stop"));
-    m_vInputs.push_back(CmdInputDef("date_restart").string_().required().description("Date restart"));
-    m_vInputs.push_back(CmdInputDef("organizators").string_().required().description("Organizators"));
+    m_vInputs.push_back(CmdInputDef("gameid").integer_().required().description("Game ID"));
+    m_vInputs.push_back(CmdInputDef("admin_password").string_().required().description("Admin Password"));
 }
 
 QString CmdGameDeleteHandler::cmd(){
@@ -43,7 +34,7 @@ const QVector<CmdInputDef> &CmdGameDeleteHandler::inputs(){
 };
 
 QString CmdGameDeleteHandler::description(){
-    return "Delete the game";
+    return "Remove game and all quests";
 }
 
 QStringList CmdGameDeleteHandler::errors(){
@@ -52,10 +43,111 @@ QStringList CmdGameDeleteHandler::errors(){
 }
 
 void CmdGameDeleteHandler::handle(QWebSocket *pClient, IWebSocketServer *pWebSocketServer, QString m, QJsonObject obj){
-    // TODO
 
-    pWebSocketServer->sendMessageError(pClient, cmd(), m, Errors::NotImplementedYet());
-    return;
+	int nGameID = obj["gameid"].toInt();
+	QString sAdminPassword = obj["admin_password"].toString();
+	
+	IUserToken *pUserToken = pWebSocketServer->getUserToken(pClient);
+    int nUserID = pUserToken->userid();
+    
+	QSqlDatabase db = *(pWebSocketServer->database());
+	
+	{
+		QSqlQuery query(db);
+		query.prepare("SELECT * FROM users WHERE id = :userid");
+		query.bindValue(":userid", nUserID);
+		if(!query.exec()){
+			pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(500, query.lastError().text()));
+			return;
+		}
+
+		QString sPass = "";
+		QString sEmail = "";
+
+		if (query.next()) {
+			QSqlRecord record = query.record();
+			sEmail = record.value("email").toString();
+			sPass = record.value("pass").toString();
+		}else{
+			pWebSocketServer->sendMessageError(pClient, cmd(), m, Errors::NotFound("user"));
+			return;
+		}
+
+		if(sAdminPassword != sPass){
+			pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(401, "Wrong password"));
+			return;
+		}
+	}
+
+	{
+		QSqlQuery query(db);
+		query.prepare("SELECT * FROM games WHERE id = :gameid");
+		query.bindValue(":gameid", nGameID);
+		
+		if(!query.exec()){
+			pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(500, query.lastError().text()));
+			return;
+		}
+		if(!query.next()) {
+			pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(404, "Game not found"));	
+			return;
+		}
+	}
+
+	// delete from users_games
+	{
+		QSqlQuery query_del(db);
+		query_del.prepare("DELETE FROM users_games WHERE gameid = :gameid");
+		query_del.bindValue(":gameid", nGameID);
+		if(!query_del.exec()){
+			pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(500, query_del.lastError().text()));
+			return;
+		}
+	}
+	
+	// delete from users_quests_answers
+	{
+		QSqlQuery query_del(db);
+		query_del.prepare("DELETE FROM users_quests_answers WHERE questid IN (SELECT idquest FROM quest q WHERE q.gameid = :gameid)");
+		query_del.bindValue(":gameid", nGameID);
+		if(!query_del.exec()){
+			pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(500, query_del.lastError().text()));
+			return;
+		}
+	}
+	
+	// delete from users_quests
+	{
+		QSqlQuery query_del(db);
+		query_del.prepare("DELETE FROM users_quests WHERE questid IN (SELECT idquest FROM quest q WHERE q.gameid = :gameid)");
+		query_del.bindValue(":gameid", nGameID);
+		if(!query_del.exec()){
+			pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(500, query_del.lastError().text()));
+			return;
+		}
+	}
+	
+	// delete from quests
+	{
+		QSqlQuery query_del(db);
+		query_del.prepare("DELETE FROM quest WHERE gameid = :gameid");
+		query_del.bindValue(":gameid", nGameID);
+		if(!query_del.exec()){
+			pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(500, query_del.lastError().text()));
+			return;
+		}
+	}
+
+	// delete from games
+	{
+		QSqlQuery query_del(db);
+		query_del.prepare("DELETE FROM games WHERE id = :gameid");
+		query_del.bindValue(":gameid", nGameID);
+		if(!query_del.exec()){
+			pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(500, query_del.lastError().text()));
+			return;
+		}
+	}
 
     QJsonObject jsonData;
 	jsonData["cmd"] = QJsonValue(cmd());
