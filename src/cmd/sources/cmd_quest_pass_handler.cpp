bd50a20d1966ef0a9c255c4d33790818fe9375be
Evgenii Sopov <mrseakg@gmail.com>
2017-10-26 21:56:34 +0700

    Fixed quest_pass

--- src/cmd/sources/cmd_quest_pass_handler.cpp
+++ src/cmd/sources/cmd_quest_pass_handler.cpp
@@ -87,7 +87,7 @@ void CmdQuestPassHandler::handle(QWebSocket *pClient, IWebSocketServer *pWebSock
 
     {
         QSqlQuery query(db);
-        query.prepare("SELECT * FROM games WHERE id = :gameid AND date_stop > NOW()");
+        query.prepare("SELECT * FROM games WHERE id = 2 AND (NOW() < date_stop OR NOW() > date_restart)");
         query.bindValue(":gameid", nGameID);
         if(!query.exec()){
             pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(500, query.lastError().text()));
@@ -95,7 +95,7 @@ void CmdQuestPassHandler::handle(QWebSocket *pClient, IWebSocketServer *pWebSock
         }
 
         if (!query.next()) {
-            pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(403, "Game ended"));
+            pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(403, "Game ended. Please "));
             return;
         }
     }
