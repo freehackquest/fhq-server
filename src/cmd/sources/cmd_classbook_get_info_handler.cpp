bfb8e52a7c5542aa3e43bfe051f6fb07dafb261a
Evgenii Sopov <mrseakg@gmail.com>
2017-10-27 23:17:06 +0700

    Small imrove classbook_get_info

--- src/cmd/sources/cmd_classbook_get_info_handler.cpp
+++ src/cmd/sources/cmd_classbook_get_info_handler.cpp
@@ -46,27 +46,51 @@ void CmdClassbookGetInfoHandler::handle(QWebSocket *pClient, IWebSocketServer *p
 
     int classbookid = obj["classbookid"].toInt();
 
-    QJsonObject info;
+    QJsonObject jsonData;
 
-    QSqlQuery query(db);
-    query.prepare("SELECT name, parentid, content FROM classbook WHERE id=:classbookid");
-    query.bindValue(":classbookid", classbookid);
-    query.exec();
-    if (query.next()) {
-        QSqlRecord record = query.record();
-        info["classbookid"] = classbookid;
-        info["parentid"] = record.value("parentid").toString();
-        info["name"] = record.value("name").toString();
-        info["content"] = record.value("content").toString();
-    }else{
-        pWebSocketServer->sendMessageError(pClient, cmd(), m, Errors::NotFound("article"));
-        return;
+    {
+        QSqlQuery query(db);
+        query.prepare("SELECT name, parentid, content FROM classbook WHERE id=:classbookid");
+        query.bindValue(":classbookid", classbookid);
+        if (!query.exec()){
+            pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(500, query.lastError().text()));
+            return;
+        }
+        if (query.next()) {
+            QSqlRecord record = query.record();
+            jsonData["classbookid"] = classbookid;
+            jsonData["parentid"] = record.value("parentid").toString();
+            jsonData["name"] = record.value("name").toString();
+            jsonData["content"] = record.value("content").toString();
+        }else{
+            pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(404, "not found classbook article with this id"));
+            return;
+        }
     }
 
-    QJsonObject jsonData;
-    jsonData["cmd"] = QJsonValue(cmd());
-    jsonData["m"] = QJsonValue(m);
-    jsonData["result"] = QJsonValue("DONE");
-    jsonData["data"] = info;
-    pWebSocketServer->sendMessage(pClient, jsonData);
+    // load langs
+    // TODO load to cache
+    {
+        QJsonObject jsonLangs;
+        QSqlQuery query(db);
+        query.prepare("SELECT id, lang FROM classbook_localization WHERE id=:classbookid");
+        query.bindValue(":classbookid", classbookid);
+        if (!query.exec()){
+            pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(500, query.lastError().text()));
+            return;
+        }
+        while(query.next()) {
+            QSqlRecord record = query.record();
+            QString lang = record.value("lang").toString();
+            jsonLangs[lang] = record.value("id").toInt();
+        }
+        jsonData["langs"] = jsonLangs;
+    }
+
+    QJsonObject jsonResponse;
+    jsonResponse["cmd"] = QJsonValue(cmd());
+    jsonResponse["m"] = QJsonValue(m);
+    jsonResponse["result"] = QJsonValue("DONE");
+    jsonResponse["data"] = jsonData;
+    pWebSocketServer->sendMessage(pClient, jsonResponse);
 }
