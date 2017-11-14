f308fa0c345edc1200a190250020048af33b84fd
Evgenii Sopov <mrseakg@gmail.com>
2017-11-14 22:23:24 +0700

    Fixed handler users

--- src/cmd_users/sources/cmd_users_handler.cpp
+++ src/cmd_users/sources/cmd_users_handler.cpp
@@ -4,6 +4,8 @@
 CmdUsersHandler::CmdUsersHandler(){
 	m_vInputs.push_back(CmdInputDef("filter_text").string_().optional().description("Filter by user email or nick"));
 	m_vInputs.push_back(CmdInputDef("filter_role").string_().optional().description("Filter by user role"));
+    m_vInputs.push_back(CmdInputDef("onpage").integer_().optional().description("On page"));
+    m_vInputs.push_back(CmdInputDef("page").integer_().optional().description("page"));
 }
 
 QString CmdUsersHandler::cmd(){
@@ -51,6 +53,10 @@ void CmdUsersHandler::handle(QWebSocket *pClient, IWebSocketServer *pWebSocketSe
 	QStringList filters;
 	QMap<QString,QString> filter_values;
 
+    int nPage = 0;
+    int nOnPage = 5;
+    int nCount = 0;
+
 	if(obj.contains("filter_text")){
 		QString text = obj["filter_text"].toString().trimmed();
 		if(text != ""){
@@ -67,36 +73,86 @@ void CmdUsersHandler::handle(QWebSocket *pClient, IWebSocketServer *pWebSocketSe
 		}
 	}
 
+    if(obj.contains("page")){
+        nPage = obj["page"].toInt();
+    }
+
+    if(obj.contains("onpage")){
+        nOnPage = obj["onpage"].toInt();
+    }
+
 	QJsonArray users;
 	QSqlDatabase db = *(pWebSocketServer->database());
-	QSqlQuery query(db);
 	QString where = filters.join(" AND "); 
 	if(where.length() > 0){
 		where = "WHERE " + where;
 	}
-	query.prepare("SELECT * FROM users " + where + " ORDER BY dt_last_login DESC");
-	foreach(QString key, filter_values.keys() ){
-		query.bindValue(key, filter_values.value(key));
-	}
-	query.exec();
-	while (query.next()) {
-		QSqlRecord record = query.record();
-		int userid = record.value("id").toInt();
-		QString uuid = record.value("uuid").toString();
-		QString email = record.value("email").toString();
-		QString nick = record.value("nick").toString();
-		QJsonObject user;
-		user["id"] = userid;
-		user["uuid"] = uuid;
-		user["nick"] = nick;
-		user["email"] = email;
-		users.push_back(user);
-	}
+
+    // calculate count
+    {
+        QSqlQuery query(db);
+        query.prepare("SELECT COUNT(*) cnt FROM users " + where);
+        foreach(QString key, filter_values.keys() ){
+            query.bindValue(key, filter_values.value(key));
+        }
+        if(!query.exec()){
+            pWebSocketServer->sendMessageError(pClient, cmd(), m, Error(500, query.lastError().text()));
+            return;
+        }
+        if(query.next()) {
+            QSqlRecord record = query.record();
+            nCount = record.value("cnt").toInt();
+        }
+    }
+
+    // users
+    {
+        QSqlQuery query(db);
+        query.prepare("SELECT * FROM users " + where + " ORDER BY dt_last_login DESC LIMIT " + QString::number(nPage*nOnPage) + "," + QString::number(nOnPage));
+        foreach(QString key, filter_values.keys() ){
+            query.bindValue(key, filter_values.value(key));
+        }
+        query.exec();
+        while (query.next()) {
+            QSqlRecord record = query.record();
+            int userid = record.value("id").toInt();
+            QString sUuid = record.value("uuid").toString();
+            QString sEmail = record.value("email").toString();
+            QString sNick = record.value("nick").toString().toHtmlEscaped();
+            QString sUniversity = record.value("university").toString().toHtmlEscaped();
+            int nRating = record.value("rating").toInt();
+            QString sCreated = record.value("dt_created").toString().toHtmlEscaped();
+            QString sDTLastLogin = record.value("dt_last_login").toString().toHtmlEscaped();
+            QString sLastIP = record.value("last_ip").toString().toHtmlEscaped();
+            QString sCountry = record.value("country").toString().toHtmlEscaped();
+            QString sRegion = record.value("region").toString().toHtmlEscaped();
+            QString sCity = record.value("city").toString().toHtmlEscaped();
+            QString sRole = record.value("role").toString().toHtmlEscaped();
+            QJsonObject user;
+            user["id"] = userid;
+            user["uuid"] = sUuid;
+            user["nick"] = sNick;
+            user["email"] = sEmail;
+            user["created"] = sCreated;
+            user["dt_last_login"] = sDTLastLogin;
+            user["last_ip"] = sLastIP;
+            user["university"] = sUniversity;
+            user["rating"] = nRating;
+            user["country"] = sCountry;
+            user["region"] = sRegion;
+            user["city"] = sCity;
+            user["role"] = sRole;
+            users.push_back(user);
+        }
+    }
 
 	QJsonObject jsonData;
 	jsonData["cmd"] = QJsonValue(cmd());
 	jsonData["result"] = QJsonValue("DONE");
 	jsonData["m"] = QJsonValue(m);
 	jsonData["data"] = users;
+    jsonData["onpage"] = nOnPage;
+    jsonData["page"] = nPage;
+    jsonData["count"] = nCount;
 	pWebSocketServer->sendMessage(pClient, jsonData);
 }
