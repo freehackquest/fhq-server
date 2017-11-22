686a8a28480a2afa6ddb56339034c0e55eabc9dd
Igor Polyakov <igor.polyakov@elecard.ru>
2017-11-22 19:59:01 +0700

    update #33. add empty test for static analizing feature

---
+++ src/unit_tests/sources/unit_static_analizing_text.cpp
@@ -0,0 +1,14 @@
+#include <unit_static_analizing_text.h>
+#include <utils_static_analizing_text.h>
+
+QString UnitTestStaticAnalizingText::name(){
+	return "UnitTestStaticAnalizingText";
+}
+
+void UnitTestStaticAnalizingText::init(){
+}
+
+bool UnitTestStaticAnalizingText::run(){
+
+    return 0;
+}
