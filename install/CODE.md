86493ee0781ac63ea5d768b38a3559eb26b9d5df
Evgenii Sopov <mrseakg@gmail.com>
2018-04-02 19:51:21 +0700

    Added code help

---
+++ install/CODE.md
@@ -0,0 +1,11 @@
+
+# CODE
+
+Statistics about lines in sources
+
+## Install requirements
+
+```
+$ cd install/CODE
+$ ./stats.sh
+```
---
+++ install/CODE/stats.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CPP_LINES_ALL=$(( find ../../fhq-server/src -name '*.cpp' -print0 | xargs -0 cat ) | wc -l)
+CPP_LINES_SMTP=$(( find ../../fhq-server/src/3rdParty -name '*.cpp' -print0 | xargs -0 cat ) | wc -l)
+
+CPP_LINES_TMP=0
+if [ -d "../../fhq-server/src/tmp" ]; then
+	CPP_LINES_TMP=$(( find ../../fhq-server/src/tmp -name '*.cpp' -print0 | xargs -0 cat ) | wc -l)
+fi
+
+
+CPP_LINES=$(expr $CPP_LINES_ALL - $CPP_LINES_SMTP - $CPP_LINES_TMP)
+echo "All lines in cpp files: $CPP_LINES"
+
+# TODO another statistics
+
+
+
+
+
