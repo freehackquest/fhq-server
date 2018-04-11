d586b9b373c674c2913b655cca971d79a0c6cdb9
Evgenii Sopov <mrseakg@gmail.com>
2018-04-12 00:09:13 +0700

    Added name to MpdelLeak

--- fhq-server/src/models/include/model_leak.h
+++ fhq-server/src/models/include/model_leak.h
@@ -1,9 +1,15 @@
 #ifndef MODEL_LEAK_H
 #define MODEL_LEAK_H
 
+#include <string>
+
 class ModelLeak {
     public:
         ModelLeak();
+        const std::string &name();
+    private:
+        std::string TAG;
+        std::string m_sName;
 };
 
 #endif // MODEL_LEAK_H
