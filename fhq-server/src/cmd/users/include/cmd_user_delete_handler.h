427705fd6d4c8aa0fba0e1300e9875f35b05cfba
Evgenii Sopov <mrseakg@gmail.com>
2018-04-15 19:13:04 +0700

    Fixed user_delete inherit by CmdHandlerBase

--- fhq-server/src/cmd/users/include/cmd_user_delete_handler.h
+++ fhq-server/src/cmd/users/include/cmd_user_delete_handler.h
@@ -4,20 +4,10 @@
 #include <iserver.h>
 #include <cmd_handlers.h>
 
-class CmdUserDeleteHandler : public ICmdHandler {
-
+class CmdUserDeleteHandler : public CmdHandlerBase {
     public:
         CmdUserDeleteHandler();
-        virtual std::string cmd();
-        virtual std::string description();
-        virtual const ModelCommandAccess &access();
-        virtual const std::vector<CmdInputDef> &inputs();
         virtual void handle(ModelRequest *pRequest);
-
-    private:
-        QString TAG;
-        ModelCommandAccess m_modelCommandAccess;
-        std::vector<CmdInputDef> m_vInputs;
 };
 
 REGISTRY_CMD(CmdUserDeleteHandler)
