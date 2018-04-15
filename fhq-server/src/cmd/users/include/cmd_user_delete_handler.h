68573a9acdc9c4aa59b5466c6fbe67ce82749e2d
Evgenii Sopov <mrseakg@gmail.com>
2018-04-15 19:14:52 +0700

    Fixed compile error

--- fhq-server/src/cmd/users/include/cmd_user_delete_handler.h
+++ fhq-server/src/cmd/users/include/cmd_user_delete_handler.h
@@ -2,7 +2,7 @@
 #define CMD_USER_DELETE_HANDLER_H
 
 #include <iserver.h>
-#include <cmd_handlers.h>
+#include <cmd_handler_base.h>
 
 class CmdUserDeleteHandler : public CmdHandlerBase {
     public:
