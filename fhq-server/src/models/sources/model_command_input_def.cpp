ac681c0aca99d2283e3e085cb27de5fdff744b38
Evgenii Sopov <mrseakg@gmail.com>
2018-04-27 19:17:46 +0700

    Prepare model_command_input_def

--- fhq-server/src/models/sources/model_command_input_def.cpp
+++ fhq-server/src/models/sources/model_command_input_def.cpp
@@ -8,65 +8,115 @@ ModelCommandInputDef::ModelCommandInputDef(){
 
 // ---------------------------------------------------------------------
 
-CmdInputDef *ModelCommandInputDef::requiredString(const std::string &sName, const std::string &sDescription){
-    CmdInputDef *pStringDef = new CmdInputDef(sName);
-    pStringDef->string_().description(sDescription).required();
-    m_vInputsDef.insert(std::pair<std::string, CmdInputDef*>(sName,pStringDef));
-    return pStringDef;
+CmdInputDef &ModelCommandInputDef::requiredString(const std::string &sName, const std::string &sDescription){
+    CmdInputDef pStringDef(sName);
+    pStringDef.string_().description(sDescription).required();
+    m_vInputsDef.insert(std::pair<std::string, CmdInputDef>(sName,pStringDef));
+    return m_vInputsDef[sName];;
 }
 
 // ---------------------------------------------------------------------
 
-CmdInputDef *ModelCommandInputDef::optionalString(const std::string &sName, const std::string &sDescription){
-    CmdInputDef *pStringDef = new CmdInputDef(sName);
-    pStringDef->string_().description(sDescription).optional();
-    m_vInputsDef.insert(std::pair<std::string, CmdInputDef*>(sName,pStringDef));
-    return pStringDef;
+CmdInputDef &ModelCommandInputDef::optionalString(const std::string &sName, const std::string &sDescription){
+    CmdInputDef pStringDef(sName);
+    pStringDef.string_().description(sDescription).optional();
+    m_vInputsDef.insert(std::pair<std::string, CmdInputDef>(sName,pStringDef));
+    return m_vInputsDef[sName];
 }
 
 // ---------------------------------------------------------------------
 
-CmdInputDef *ModelCommandInputDef::defineBoolean(const std::string &sName, const std::string &sDescription){
-    CmdInputDef *pBooleanDef = new CmdInputDef(sName);
-    pBooleanDef->bool_().description(sDescription);
-    m_vInputsDef.insert(std::pair<std::string, CmdInputDef*>(sName,pBooleanDef));
-    return pBooleanDef;
+CmdInputDef &ModelCommandInputDef::requiredBoolean(const std::string &sName, const std::string &sDescription){
+    CmdInputDef pBooleanDef(sName);
+    pBooleanDef.bool_().description(sDescription).required();
+    m_vInputsDef.insert(std::pair<std::string, CmdInputDef>(sName,pBooleanDef));
+    return m_vInputsDef[sName];
 }
 
 // ---------------------------------------------------------------------
 
-CmdInputDef *ModelCommandInputDef::defineEnum(const std::string &sName, const std::string &sDescription){
-    CmdInputDef *pEnumDef = new CmdInputDef(sName);
-    pEnumDef->enum_(QStringList()).description(sDescription);
-    m_vInputsDef.insert(std::pair<std::string, CmdInputDef*>(sName,pEnumDef));
-    return pEnumDef;
+CmdInputDef &ModelCommandInputDef::optionalBoolean(const std::string &sName, const std::string &sDescription){
+    CmdInputDef pBooleanDef(sName);
+    pBooleanDef.bool_().description(sDescription).optional();
+    m_vInputsDef.insert(std::pair<std::string, CmdInputDef>(sName,pBooleanDef));
+    return m_vInputsDef[sName];
 }
 
 // ---------------------------------------------------------------------
 
-CmdInputDef *ModelCommandInputDef::defineUUID(const std::string &sName, const std::string &sDescription){
-    CmdInputDef *pUuidDef = new CmdInputDef(sName);
-    pUuidDef->uuid_().description(sDescription);
-    m_vInputsDef.insert(std::pair<std::string, CmdInputDef*>(sName,pUuidDef));
-    return pUuidDef;
+CmdInputDef &ModelCommandInputDef::requiredEnum(const std::string &sName, const std::string &sDescription){
+    CmdInputDef pEnumDef(sName);
+    pEnumDef.enum_(QStringList()).description(sDescription).required();
+    m_vInputsDef.insert(std::pair<std::string, CmdInputDef>(sName,pEnumDef));
+    return m_vInputsDef[sName];
 }
 
 // ---------------------------------------------------------------------
 
-CmdInputDef *ModelCommandInputDef::defineInteger(const std::string &sName, const std::string &sDescription){
-    CmdInputDef *pIntegerDef = new CmdInputDef(sName);
-    pIntegerDef->integer_().description(sDescription);
-    m_vInputsDef.insert(std::pair<std::string, CmdInputDef*>(sName,pIntegerDef));
-    return pIntegerDef;
+CmdInputDef &ModelCommandInputDef::optionalEnum(const std::string &sName, const std::string &sDescription){
+    CmdInputDef pEnumDef(sName);
+    pEnumDef.enum_(QStringList()).description(sDescription).optional();
+    m_vInputsDef.insert(std::pair<std::string, CmdInputDef>(sName,pEnumDef));
+    return m_vInputsDef[sName];
 }
 
 // ---------------------------------------------------------------------
 
-CmdInputDef *ModelCommandInputDef::defineAny(const std::string &sName, const std::string &sDescription){
-    CmdInputDef *pAnyDef = new CmdInputDef(sName);
-    pAnyDef->any_().description(sDescription);
-    m_vInputsDef.insert(std::pair<std::string, CmdInputDef*>(sName,pAnyDef));
-    return pAnyDef;
+CmdInputDef &ModelCommandInputDef::requiredUUID(const std::string &sName, const std::string &sDescription){
+    CmdInputDef pUuidDef(sName);
+    pUuidDef.uuid_().description(sDescription).required();
+    m_vInputsDef.insert(std::pair<std::string, CmdInputDef>(sName,pUuidDef));
+    return m_vInputsDef[sName];
 }
 
 // ---------------------------------------------------------------------
+
+CmdInputDef &ModelCommandInputDef::optionalUUID(const std::string &sName, const std::string &sDescription){
+    CmdInputDef pUuidDef(sName);
+    pUuidDef.uuid_().description(sDescription).optional();
+    m_vInputsDef.insert(std::pair<std::string, CmdInputDef>(sName,pUuidDef));
+    return m_vInputsDef[sName];
+}
+
+// ---------------------------------------------------------------------
+
+CmdInputDef &ModelCommandInputDef::requiredEmail(const std::string &sName, const std::string &sDescription){
+    CmdInputDef def(sName);
+    def.email_().description(sDescription).required();
+    m_vInputsDef.insert(std::pair<std::string, CmdInputDef>(sName,def));
+    return m_vInputsDef[sName];
+}
+
+// ---------------------------------------------------------------------
+
+CmdInputDef &ModelCommandInputDef::optionalEmail(const std::string &sName, const std::string &sDescription){
+    CmdInputDef def(sName);
+    def.email_().description(sDescription).optional();
+    m_vInputsDef.insert(std::pair<std::string, CmdInputDef>(sName,def));
+    return m_vInputsDef[sName];
+}
+
+// ---------------------------------------------------------------------
+
+CmdInputDef &ModelCommandInputDef::requiredInteger(const std::string &sName, const std::string &sDescription){
+    CmdInputDef pIntegerDef(sName);
+    pIntegerDef.integer_().description(sDescription).required();
+    m_vInputsDef.insert(std::pair<std::string, CmdInputDef>(sName,pIntegerDef));
+    return m_vInputsDef[sName];
+}
+
+// ---------------------------------------------------------------------
+
+CmdInputDef &ModelCommandInputDef::optionalInteger(const std::string &sName, const std::string &sDescription){
+    CmdInputDef pIntegerDef(sName);
+    pIntegerDef.integer_().description(sDescription).optional();
+    m_vInputsDef.insert(std::pair<std::string, CmdInputDef>(sName,pIntegerDef));
+    return m_vInputsDef[sName];
+}
+
+// ---------------------------------------------------------------------
+
+void ModelCommandInputDef::push_back(const CmdInputDef &cmdInputDef){
+	std::string sName = cmdInputDef.getName();
+	m_vInputsDef.insert(std::pair<std::string, CmdInputDef>(sName,cmdInputDef));
+}
