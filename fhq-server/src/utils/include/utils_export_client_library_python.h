bee3d7845b0653402540d0c0557fb8757e6c48ba
Evgenii Sopov <mrseakg@gmail.com>
2018-07-01 02:26:39 +0700

    Redesign export to python library

--- fhq-server/src/utils/include/utils_export_client_library_python.h
+++ fhq-server/src/utils/include/utils_export_client_library_python.h
@@ -4,6 +4,11 @@
 class UtilsExportClientLibraryPython {
     public:
         static void exportLib();
+    private:
+        static void exportSetupPy();
+        static void exportAPImd();
+        static void export__init__py();
+        static void exportPrepareDirs();
 };
 
 #endif // UTILS_EXPORT_CLIENT_LIBRARY_PYTHON_H
