31b5f6da262a6b17aec4adf6a20f963a00bd6b64
Evgenii Sopov <mrseakg@gmail.com>
2018-07-07 01:28:20 +0700

    Updated fhqtest.py

--- tests/fhqtest.py
+++ tests/fhqtest.py
@@ -4,8 +4,11 @@
 import sys
 import libfhqcli
 
-admin_session = None;
+admin_session = None
+loggined = False
+game_uuid = "00000000-0000-0000-0000-000000000000"
 game = None;
+
 
 def alert(check, msg):
     if(check == True):
@@ -15,18 +18,39 @@ def alert(check, msg):
 def init_enviroment():
     global admin_session
     global game
+    global game_uuid
     admin_session = libfhqcli.FHQCli("ws://localhost:1234/");
     resp = admin_session.login({"email": "admin", "password": "admin"});
     alert(resp == None, 'Could not login as admin (1)');
     alert(resp['result'] == 'FAIL', 'Could not login as admin (2)');
-
-    # admin_session.game_create()
-    resp = admin_session.game_create({"uuid": "", "name": "", "description": "", "state": "", "form": "", "type": "", "date_start": "", "date_stop": "", "date_restart": "", "organizators": ""});
-    alert(resp == None, 'Could not send message (2)');
-    alert(resp['result'] == 'FAIL', 'Could not create test game');
+    
+    loggined = True
+    game = admin_session.game_info({"uuid": game_uuid})
+    alert(resp == None, 'Could not get test game (2)');
+    print(game);
+    if game['result'] == 'FAIL':
+        game = admin_session.game_create({
+            "uuid": game_uuid,
+            "name": "test",
+            "description": "test",
+            "state": "",
+            "form": "",
+            "type": "",
+            "date_start": "2000-01-01 00:00:00",
+            "date_stop": "2001-01-01 00:00:00",
+            "date_restart": "2002-01-01 00:00:00",
+            "organizators": "test"
+        });
+        alert(game == None, 'Could not send message (2)');
+        alert(game['result'] == 'FAIL', 'Could not create test game ' + str(resp));
 
 def deinit_enviroment():
     global admin_session
     global game
+    global game_uuid
+
+    # try remove all test objects
+    if loggined:
+        admin_session.game_delete({"uuid": game_uuid, "admin_password": "admin"})
 
     admin_session.close();
