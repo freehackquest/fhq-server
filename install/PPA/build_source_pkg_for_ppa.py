5c56ac6ad138c3cdb150a6403c1f6cde3d3a9b72
Evgenii Sopov <mrseakg@gmail.com>
2018-07-19 11:11:32 +0700

    Updated script for uploading ppa

--- install/PPA/build_source_pkg_for_ppa.py
+++ install/PPA/build_source_pkg_for_ppa.py
@@ -34,23 +34,68 @@ filedata = f.read()
 f.close()
 
 # add_definitions(-DVERSION_STRING="0.2.4")
-m = re.search('DVERSION_STRING=\"(.+)\"', filedata)
+m = re.search('DFHQSRV_VERSION=\"(.+)\"', filedata)
 if m:
-  current_version = m.group(1)
+    current_version = m.group(1)
 
 print ("Current version: " + current_version);
-new_version = raw_input("New version: ")
 
+'''
+new_version = raw_input("New version: ")
 newdata = filedata.replace(current_version,new_version)
 f = open(cmakelist,'w')
 f.write(newdata)
-f.close()
+f.close()'''
+
+# parse CHANGELOG.md
+changelog_list = []
+version_logs = {'version': '', 'dt': '', 'logs': []}
+lines = [line.rstrip('\n') for line in open('../../CHANGELOG.md')]
+for li in lines:
+	m = re.search('[ ]*##[ ]+\[(\d+\.\d+\.\d+)\][ ]*\((.*)\).*', li)
+	if m:
+		if version_logs['version'] != '':
+			changelog_list.append(version_logs)
+			version_logs = {'version': '', 'dt': '', 'logs': []}
+		ver = m.group(1)
+		dt = m.group(2)
+		version_logs['version'] = ver;
+		version_logs['dt'] = dt
+	
+	m = re.search('[ ]*-[ ]*(.*)', li)
+	if m:
+		line_log = m.group(1)
+		version_logs['logs'].append(line_log)
+
+if version_logs['version'] != '':
+	changelog_list.append(version_logs)
+	version_logs = {'version': '', 'dt': '', 'logs': []}
+
+
+# print(li)
+# print(lines)
+# m = re.search('DFHQSRV_VERSION=\"(.+)\"', filedata)
+#if m:
+#  current_version = m.group(1)
 
 
 # Prepare changelog
 changelog="../../fhq-server/debian/changelog"
 f = open(changelog,'w')
-f.write("fhq-server (" + new_version + "-" + ppa_name_ + ") " + dist_name_ + "; urgency=low\n\n")
+
+for li in changelog_list:
+	f.write("fhq-server (" + li['version'] + "-" + ppa_name_ + ") " + dist_name_ + "; urgency=low\n\n")
+	for li_log in li['logs']:
+		li_log = li_log.strip()
+		if li_log != '':
+			f.write("  * " + li_log + "\n");
+	f.write("\n")
+	f.write(" -- Free Hack Quest <freehackquest@gmail.com>  " + li['dt']) # 2 space!!!
+	f.write("\n")
+f.close()
+
+'''
+f.write("fhq-server (" + current_version + "-" + ppa_name_ + ") " + dist_name_ + "; urgency=low\n\n")
 changelog_message = raw_input("Changelog message (or empty - end): ");
 while changelog_message != "":
 	f.write("  * " + changelog_message + "\n");
@@ -60,11 +105,12 @@ f.write("\n")
 dt=subprocess.Popen(['date', '-R'], stdout=subprocess.PIPE).communicate()[0]
 
 f.write(" -- Free Hack Quest <freehackquest@gmail.com>  " + dt)
+'''
+
 f.close()
 
-
 subprocess.call("./clean_sources_ppa.sh");
-os.system("cd ../../ && tar -acf fhq-server_" + new_version+ "-" + ppa_name_ + ".orig.tar.gz fhq-server");
+os.system("cd ../../ && tar -acf fhq-server_" + current_version + "-" + ppa_name_ + ".orig.tar.gz fhq-server");
 os.system("cd ../../fhq-server && debuild -S -sa");
 
 
@@ -72,4 +118,4 @@ sys.stdout.write("Are you want try upload source package to ppa.launchpad? [y/n]
 ask_upload_ = raw_input().lower()
 
 if ask_upload_ == "y":
-	os.system("cd ../../ && dput ppa:freehackquest/fhq-server fhq-server_" + new_version + "-" + ppa_name_ + "_source.changes");
+	os.system("cd ../../ && dput ppa:freehackquest/fhq-server fhq-server_" + current_version + "-" + ppa_name_ + "_source.changes");
