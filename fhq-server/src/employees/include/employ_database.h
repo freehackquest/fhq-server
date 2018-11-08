cee8b175406f64d3364ee1744f4133a7971c9102
Evgenii Sopov <mrseakg@gmail.com>
2018-11-08 11:52:00 +0700

    Prepare to use the original mysql driver

--- fhq-server/src/employees/include/employ_database.h
+++ fhq-server/src/employees/include/employ_database.h
@@ -5,6 +5,7 @@
 #include <employees.h>
 
 #include <QMutex>
+#include <mysql/mysql.h>
 
 class EmployDatabase : public EmployBase {
     public:
@@ -13,14 +14,24 @@ class EmployDatabase : public EmployBase {
        virtual bool init();
        QSqlDatabase *database();
        bool manualCreateDatabase(const std::string& sRootPassword, std::string& sError);
+       MYSQL *db();
+
 	private:
 		std::string TAG;
+
+        // new
+        std::map<std::string, MYSQL *> m_mapConnections;
+
         // db two connections
 		QMutex m_mtxSwapConenctions;
+
+        // old
 		QMap<long long, ModelDatabaseConnection *> m_mDatabaseConnections;
 		QMap<long long, ModelDatabaseConnection *> m_mDatabaseConnections_older;
 		ModelDatabaseConnection *m_pDBConnection;
 		ModelDatabaseConnection *m_pDBConnection_older;
+
+
 };
 
 #endif // EMPLOY_DATABASE_H
