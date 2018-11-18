65630122109b26b2b318f5a6746fc4f0359e5f19
Evgenii Sopov <mrseakg@gmail.com>
2018-11-18 17:27:57 +0700

    Fixed running test_games for a python3

--- tests/test_games.py
+++ tests/test_games.py
@@ -65,9 +65,9 @@ try:
             bFound = True
 
     if bFound != True:
-        fhqtest.log_err("Game not found in list");
+        fhqtest.log_err("Game not found in list")
     else:
-        fhqtest.print_success("Game found in list");
+        fhqtest.print_success("Game found in list")
 
     # test update game name
     fhqtest.print_bold("Update game... ")
@@ -75,41 +75,42 @@ try:
         "uuid": game_uuid1,
         "name": "game2",
     });
-    fhqtest.alert(game_updt == None, 'Could not get response');
+    fhqtest.alert(game_updt == None, 'Could not get response')
     if game_updt['result'] == 'FAIL':
         fhqtest.log_err(game_updt['error'])
     else:
-        game1 = fhqtest.admin_session.game_info({ "uuid": game_uuid1 });
+        game1 = fhqtest.admin_session.game_info({ "uuid": game_uuid1 })
         fhqtest.print_bold("Check name after update... ")
         fhqtest.check_response(game1, "Games list got")
         game1 = game1['data']
         if game1['name'] != "game2": # wrong  param name must be name
-            fhqtest.log_err("Game has wrong name (2)");
+            fhqtest.log_err("Game has wrong name (2)")
         else:
-            fhqtest.print_success("Game name ok");
+            fhqtest.print_success("Game name ok")
 
     # TODO upload log for test game
     fhqtest.print_bold("Game update logo test... ")
-    f = open("files/game1.png", 'r')
+    f = open("files/game1.png", 'rb')
     image_png_base64 = f.read()
     f.close()
-    img_len = len(image_png_base64);
+    img_len = len(image_png_base64)
     if img_len != 17045:
-        fhqtest.log_err("Game Logo wrong size, current " + str(img_len) + ", but expected 17045");
-    image_png_base64 = base64.b64encode(image_png_base64);
+        fhqtest.log_err("Game Logo wrong size, current " + str(img_len) + ", but expected 17045")
+    image_png_base64 = base64.b64encode(image_png_base64)
+    image_png_base64 = image_png_base64.decode("utf-8")
     game_img = fhqtest.admin_session.game_update_logo({
         "uuid": game_uuid1, # wrong must be uuid
         "image_png_base64": image_png_base64
-    });
-    fhqtest.alert(game_img == None, 'Could not get response');
+    })
+    fhqtest.alert(game_img == None, 'Could not get response')
     fhqtest.check_response(game_img, "Game Logo updated")
 
     # export
-    game_exp = fhqtest.admin_session.game_export({"uuid": game_uuid1});
-    fhqtest.alert(game_exp == None, 'Could not get response');
+    game_exp = fhqtest.admin_session.game_export({"uuid": game_uuid1})
+    fhqtest.alert(game_exp == None, 'Could not get response')
     fhqtest.check_response(game_exp, "Game exported")
     game_zip = base64.b64decode(game_exp['data']['zipfile_base64'])
-    f = open(game_exp['data']['zipfile_name'], 'w')
+    f = open(game_exp['data']['zipfile_name'], 'wb')
     f.write(game_zip)
     f.close()
     # print(game_exp)
@@ -133,8 +134,8 @@ try:
     '''
 
 except Exception as e:
-    fhqtest.log_err(str(e));
+    fhqtest.log_err(str(e))
     traceback.print_exc(file=sys.stdout)
 finally:
-    fhqtest.print_header(" < < < " + test_name + ": end ");
-    fhqtest.deinit_enviroment();
+    fhqtest.print_header(" < < < " + test_name + ": end ")
+    fhqtest.deinit_enviroment()
