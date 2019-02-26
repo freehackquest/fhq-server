342d7139a2453b8f872d405638ed7549c5916a53
keimashikai <shikamaru740@gmail.com>
2019-02-26 22:23:10 +0700

    Fixed unit for merge

--- fhq-server/src/unit_tests/unit_test_merge_text.cpp
+++ fhq-server/src/unit_tests/unit_test_merge_text.cpp
@@ -28,7 +28,7 @@ bool UnitTestMergeText::run() {
             std::vector<UtilsMergeTextRow *> arr2,
             unsigned int n
         ) : txt1(txt1), txt2(txt2), txt3(txt3), arr1(arr1), arr2(arr2), n(n) {
-            // 
+            //
         }
         std::string txt1;
         std::string txt2;
@@ -49,46 +49,46 @@ bool UnitTestMergeText::run() {
     //empty (Test №1)
     tests1.push_back(new LTest("", "", "", arr1, arr2, 0));
     //without any difference (Test №2)
-    tests1.push_back(new LTest("I\r\ngot\r\nMike", "I\r\ngot\r\nMike", "I\r\ngot\r\nMike", arr1, arr2, 0));
+    tests1.push_back(new LTest("I\ngot\nMike", "I\ngot\nMike", "I\ngot\nMike", arr1, arr2, 0));
     //with a difference in the ending (Tests №3-6)
-    tests1.push_back(new LTest("I/got/Mike", "I/got/Mike", "I/got/Nike", arr1, arr2, 1));
-    tests1.push_back(new LTest("I/got/Nike", "I/got/Mike", "I/got/Mike", arr1, arr2, 1));
-    tests1.push_back(new LTest("I/got/Nike", "I/got/Mike", "I/got/Nike", arr1, arr2, 1));
-    tests1.push_back(new LTest("I/got/Bike", "I/got/Mike", "I/got/Nike", arr1, arr2, 1));
+    tests1.push_back(new LTest("I\ngot\nMike", "I\ngot\nMike", "I\ngot\nNike", arr1, arr2, 1));
+    tests1.push_back(new LTest("I\ngot\nNike", "I\ngot\nMike", "I\ngot\nMike", arr1, arr2, 1));
+    tests1.push_back(new LTest("I\ngot\nNike", "I\ngot\nMike", "I\ngot\nNike", arr1, arr2, 1));
+    tests1.push_back(new LTest("I\ngot\nBike", "I\ngot\nMike", "I\ngot\nNike", arr1, arr2, 1));
     //with a difference in the beginning (Tests №7-10)
-    tests1.push_back(new LTest("I/got/Mike", "I/got/Mike", "You/got/Mike", arr1, arr2, 1));
-    tests1.push_back(new LTest("You/got/Mike", "I/got/Mike", "I/got/Mike", arr1, arr2, 1));
-    tests1.push_back(new LTest("You/got/Mike", "I/got/Mike", "You/got/Mike", arr1, arr2, 1));
-    tests1.push_back(new LTest("We/got/Mike", "I/got/Mike", "You/got/Mike", arr1, arr2, 1));
+    tests1.push_back(new LTest("I\ngot\nMike", "I\ngot\nMike", "You\ngot\nMike", arr1, arr2, 1));
+    tests1.push_back(new LTest("You\ngot\nMike", "I\ngot\nMike", "I\ngot\nMike", arr1, arr2, 1));
+    tests1.push_back(new LTest("You\ngot\nMike", "I\ngot\nMike", "You\ngot\nMike", arr1, arr2, 1));
+    tests1.push_back(new LTest("We\ngot\nMike", "I\ngot\nMike", "You\ngot\nMike", arr1, arr2, 1));
     //with a difference in the middle of the row (Tests №11-15)
-    tests1.push_back(new LTest("I/have/cute/cats", "I/have/cute/cats", "I/have/huge/cats", arr1, arr2, 1));
-    tests1.push_back(new LTest("I/have/huge/cats", "I/have/cute/cats", "I/have/cute/cats", arr1, arr2, 1));
-    tests1.push_back(new LTest("I/have/huge/cats", "I/have/cute/cats", "I/have/huge/cats", arr1, arr2, 1));
-    tests1.push_back(new LTest("I/have/difference/cats", "I/have/cute/cats", "I/have/huge/cats", arr1, arr2, 1));
-    tests1.push_back(new LTest("I/betray/cats", "I/have/cats", "I/love/cats", arr1, arr2, 1));
+    tests1.push_back(new LTest("I\nhave\ncute\ncats", "I\nhave\ncute\ncats", "I\nhave\nhuge\ncats", arr1, arr2, 1));
+    tests1.push_back(new LTest("I\nhave\nhuge\ncats", "I\nhave\ncute\ncats", "I\nhave\ncute\ncats", arr1, arr2, 1));
+    tests1.push_back(new LTest("I\nhave\nhuge\ncats", "I\nhave\ncute\ncats", "I\nhave\nhuge\ncats", arr1, arr2, 1));
+    tests1.push_back(new LTest("I\nhave\ndifference\ncats", "I\nhave\ncute\ncats", "I\nhave\nhuge\ncats", arr1, arr2, 1));
+    tests1.push_back(new LTest("I\nbetray\ncats", "I\nhave\ncats", "I\nlove\ncats", arr1, arr2, 1));
     //delete lines from the end of text (Tests №16-19)
-    tests1.push_back(new LTest("Go\r\nto\r\nthe\r\nschool", "Go\r\nto\r\nthe\r\nschool", "Go\r\nto", arr1, arr2, 2));
-    tests1.push_back(new LTest("Go/to", "Go/to/the/school", "Go/to/the/school", arr1, arr2, 2));
-    tests1.push_back(new LTest("Go/to/the", "Go/to/the/school", "Go/to", arr1, arr2, 2));
-    tests1.push_back(new LTest("Go/to", "Go/to/the/school", "Go/to/the", arr1, arr2, 2));
+    tests1.push_back(new LTest("Go\nto\nthe\nschool", "Go\nto\nthe\nschool", "Go\nto", arr1, arr2, 2));
+    tests1.push_back(new LTest("Go\nto", "Go\nto\nthe\nschool", "Go\nto\nthe\nschool", arr1, arr2, 2));
+    tests1.push_back(new LTest("Go\nto\nthe", "Go\nto\nthe\nschool", "Go\nto", arr1, arr2, 2));
+    tests1.push_back(new LTest("Go\nto", "Go\nto\nthe\nschool", "Go\nto\nthe", arr1, arr2, 2));
     //add lines in the end of text (Tests №20-23)
-    tests1.push_back(new LTest("Go/to", "Go/to", "Go/to/the/school", arr1, arr2, 2));
-    tests1.push_back(new LTest("Go/to/the/school", "Go/to", "Go/to", arr1, arr2, 2));
-    tests1.push_back(new LTest("Go/to/the", "Go/to", "Go/to/the/school", arr1, arr2, 2));
-    tests1.push_back(new LTest("Go/to/the/school", "Go/to", "Go/to/the", arr1, arr2, 2));
+    tests1.push_back(new LTest("Go\nto", "Go\nto", "Go\nto\nthe\nschool", arr1, arr2, 2));
+    tests1.push_back(new LTest("Go\nto\nthe\nschool", "Go\nto", "Go\nto", arr1, arr2, 2));
+    tests1.push_back(new LTest("Go\nto\nthe", "Go\nto", "Go\nto\nthe\nschool", arr1, arr2, 2));
+    tests1.push_back(new LTest("Go\nto\nthe\nschool", "Go\nto", "Go\nto\nthe", arr1, arr2, 2));
     //delete rows from the beginning and the middle of text (Tests №24-27)
-    tests1.push_back(new LTest("Go/to/the/school", "Go/to/the/school", "the/school", arr1, arr2, 2));
-    tests1.push_back(new LTest("the/school", "Go/to/the/school", "Go/to/the/school", arr1, arr2, 2));
-    tests1.push_back(new LTest("Go/to/the/school", "Go/to/the/school", "Go/to/school", arr1, arr2, 1));
-    tests1.push_back(new LTest("Go/to/school", "Go/to/the/school", "Go/to/the/school", arr1, arr2, 1));
+    tests1.push_back(new LTest("Go\nto\nthe\nschool", "Go\nto\nthe\nschool", "the\nschool", arr1, arr2, 2));
+    tests1.push_back(new LTest("the\nschool", "Go\nto\nthe\nschool", "Go\nto\nthe\nschool", arr1, arr2, 2));
+    tests1.push_back(new LTest("Go\nto\nthe\nschool", "Go\nto\nthe\nschool", "Go\nto\nschool", arr1, arr2, 1));
+    tests1.push_back(new LTest("Go\nto\nschool", "Go\nto\nthe\nschool", "Go\nto\nthe\nschool", arr1, arr2, 1));
     //add rows to the beginning and the middle of text (Tests №28-31)
-    tests1.push_back(new LTest("the/school", "the/school", "Go/to/the/school", arr1, arr2, 2));
-    tests1.push_back(new LTest("Go/to/the/school", "the/school", "the/school", arr1, arr2, 2));
-    tests1.push_back(new LTest("Go/the/school", "the/school", "the/school", arr1, arr2, 1));
-    tests1.push_back(new LTest("the/school", "the/school", "Go/the/school", arr1, arr2, 1));
+    tests1.push_back(new LTest("the\nschool", "the\nschool", "Go\nto\nthe\nschool", arr1, arr2, 2));
+    tests1.push_back(new LTest("Go\nto\nthe\nschool", "the\nschool", "the\nschool", arr1, arr2, 2));
+    tests1.push_back(new LTest("Go\nthe\nschool", "the\nschool", "the\nschool", arr1, arr2, 1));
+    tests1.push_back(new LTest("the\nschool", "the\nschool", "Go\nthe\nschool", arr1, arr2, 1));
     //complex (Tests №32-33)
-    tests1.push_back(new LTest("You/are/got/and/good", "I/got/Mike/and/this is/good/boy", "I/cure/damn/ill/cancer", arr1, arr2, 8));
-    tests1.push_back(new LTest("We/spent/the rest/of our life/on/this/course/work", "We/spent/a lot of/time/on/this/course/work", "We/gonna/spend/a lot of/funky/time/on/course/work", arr1, arr2, 6));
+    tests1.push_back(new LTest("You\nare\ngot\nand\ngood", "I\ngot\nMike\nand\nthis is\ngood\nboy", "I\ncure\ndamn\nill\ncancer", arr1, arr2, 8));
+    tests1.push_back(new LTest("We\nspent\nthe rest\nof our life\non\nthis\ncourse\nwork", "We\nspent\na lot of\ntime\non\nthis\ncourse\nwork", "We\ngonna\nspend\na lot of\nfunky\ntime\non\ncourse\nwork", arr1, arr2, 6));
 
     unsigned int nSuccess = 0;
     for (unsigned int i = 0; i < tests1.size(); i++){
@@ -154,9 +154,9 @@ bool UnitTestMergeText::run() {
     for(int i=0;i<skey.size();++i) arr3.push_back(new UtilsMergeTextRow(id[i], skey.at(i), sline.at(i)));
 
     tests2.push_back(new LTest2(
-        "You\r\nare\r\ngot\r\nand\r\ngood",
-        "I\r\ngot\r\nMike\r\nand\r\nthis is\r\ngood\r\nboy",
-        "I\r\ncure\r\ndamn\r\nill\r\ncancer",
+        "You\nare\ngot\nand\ngood",
+        "I\ngot\nMike\nand\nthis is\ngood\nboy",
+        "I\ncure\ndamn\nill\ncancer",
         arr1, arr2, arr3));
 
     std::string txt1 = tests2[0]->txt1;
@@ -201,30 +201,30 @@ bool UnitTestMergeText::run() {
 // ---------------------------------------------------------------------
 
 std::string UnitTestMergeText::text001() {
-    return "Str1\r\n"
-        "Str\r\n"
-        "Str\r\n"
-        "Str\r\n"
-        "Str2\r\n"
-        "Str3\r\n"
-        "Str\r\n"
-        "Str\r\n"
-        "Str\r\n";
+    return "Str1\n"
+        "Str\n"
+        "Str\n"
+        "Str\n"
+        "Str2\n"
+        "Str3\n"
+        "Str\n"
+        "Str\n"
+        "Str\n";
 }
 
 // ---------------------------------------------------------------------
 
 std::string UnitTestMergeText::text002() {
-    return "Str1\r\n"
-        "Str \r\n"
-        "Str\r\n"
-        "Str\r\n"
-        "Str2\r\n"
-        "Str\r\n"
-        "Str3\r\n"
-        "Str\r\n"
-        "Str\r\n"
-        "Str\r\n";
+    return "Str1\n"
+        "Str \n"
+        "Str\n"
+        "Str\n"
+        "Str2\n"
+        "Str\n"
+        "Str3\n"
+        "Str\n"
+        "Str\n"
+        "Str\n";
 }
 
 // ---------------------------------------------------------------------
