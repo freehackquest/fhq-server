303e64875231b45b9305fb32819c4b0e8ff8f843
Evgenii Sopov <mrseakg@gmail.com>
2019-05-17 15:28:52 +0700

    Implemented edit text in admin

--- web-admin/js/fhq.admin.quests.js
+++ web-admin/js/fhq.admin.quests.js
@@ -77,12 +77,16 @@ fhq.pages['quests'] = function() {
 		el.append('<hr>');
 
 		// el.append(fhq.paginator(0, r.count, r.onpage, r.page));
-		el.append('<table class="table table-striped">'
-			+ '		<thead>'
+		el.append('<table class="table table-striped table-hover">'
+			+ '		<thead class="thead-dark">'
 			+ '			<tr>'
 			+ '				<th>#</th>'
-			+ '				<th>Info</th>'
+			+ '				<th>Game</th>'
+			+ '				<th>Subject</th>'
+			+ '             <th>Score</th>'
+			+ '             <th>Name</th>'
 			+ '				<th>State</th>'
+			+ '				<th>Solved</th>'
 			+ '				<th>Actions</th>'
 			+ '			</tr>'
 			+ '		</thead>'
@@ -93,30 +97,36 @@ fhq.pages['quests'] = function() {
 
 		for(var i in r.data){
 			var q = r.data[i];
-			$('#list').append('<tr>'
+			var row_color = '';
+			if (q.state == "closed") {
+				row_color = 'table-warning';
+			}
+
+			if (q.state == "broken") {
+				row_color = 'table-danger';
+			}
+
+			$('#list').append('<tr class="' + row_color + '">'
 				+ '	<td>' + q.questid + '</td>'
-				+ '	<td>' 
-				+ "		<p>GameID: " + q.gameid+ "<p>"
-				+ "		<p>Name: " + q.name+ "<p>"
-				+ "		<p>Subject: " + q.subject+ "<p>"
-				+ "		<p>Score: +" + q.score+ "<p>"
-				+ "		<p>Solved: " + q.solved + " user(s)<p>"
-				+ '</td>'
+				+ '	<td>[game#' +  q.gameid + ']</td>'
+				+ '	<td>' +  q.subject + '</td>'
+				+ '	<td>+' +  q.score + '</td>'
+				+ '	<td>' +  q.name + '</td>'
 				+ '	<td>' + q.state + '</td>'
+				+ '	<td>' + q.solved + ' user(s)</td>'
 				+ '	<td>'
-				+ '			<div class="tbl-action-btn quest-remove" questid="' + q.questid + '"><i class="fa fa-trash"></i></div>'
-				+ '			<div class="tbl-action-btn quest-export" questid="' + q.questid + '"><i class="fa fa-download"></i></div>'
-				+ '		<p><button class="btn btn-secondary quest-edit" questid="' + q.questid + '">Edit</button></p>'
+				+ '		<div class="btn btn-primary quest-edit" questid="' + q.questid + '">Edit</div>'
+				+ '		<div class="btn btn-primary quest-export" questid="' + q.questid + '">Export</div>'
+				+ '		<div class="btn btn-secondary quest-remove" questid="' + q.questid + '">Delete</div>'
 				+ '</td>'
 				+ '</tr>'
 			);
 		}
 		
-		/*$('.game-edit').unbind().bind('click', function(){
-			var gameuuid = $(this).attr('gameuuid');
-			fhq.pages['game_edit'](gameuuid);
+		$('.quest-edit').unbind().bind('click', function(){
+			var questid = $(this).attr('questid');
+			fhq.pages['quest_edit'](questid);
 		});
-		*/
 
 		$('.quest-export').unbind().bind('click', function(){
 			var questid = $(this).attr('questid');
@@ -147,7 +157,7 @@ fhq.pages['quests'] = function() {
 	})
 }
 
-fhq.pages['quest_create'] = function(){
+fhq.pages['quest_create'] = function() {
 	$('#page_name').html('Quest Create');
 	
 	var el = $('#page_content');
@@ -299,6 +309,203 @@ fhq.createQuest = function() {
 		console.error(err);
 		$('#quest_create_error').show();
 		$('#quest_create_error').html(err.error);
+		
+	});
+};
+
+
+fhq.pages['quest_edit'] = function(questid) {
+	$('#page_name').html('Quest Edit');
+	questid = questid || fhq.pageParams['quest_edit'];
+	if (questid) {
+		questid = parseInt(questid, 10);
+	}
+	var el = $('#page_content');
+	el.html('');
+	console.log("questid: ", questid);
+	window.fhq.changeLocationState({'quest_edit':questid});
+	fhq.hideLoader();
+	
+	el.html('');
+	el.html(''
+		+ '<div class="card">'
+		+ '		<div class="card-header">' + fhq.t('Edit Quest') + '</div>'
+		+ '		<div class="card-body">'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_quest_id" class="col-sm-2 col-form-label">' + fhq.t('ID') + '</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<input type="text" readonly class="form-control" value="" id="edit_quest_id">'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_quest_count_user_solved" class="col-sm-2 col-form-label">' + fhq.t('Solved') + '</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<input type="text" readonly class="form-control" value="" id="edit_quest_count_user_solved">'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_quest_uuid" class="col-sm-2 col-form-label">' + fhq.t('UUID') + '</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<input type="text" readonly class="form-control" value="" id="edit_quest_uuid">'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_quest_gameid" class="col-sm-2 col-form-label">' + fhq.t('Game') + '</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<select class="form-control" id="edit_quest_gameid"></select>'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_name" class="col-sm-2 col-form-label">' + fhq.t('Name') + '</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<input type="text" class="form-control" value="" id="edit_quest_name">'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_text" class="col-sm-2 col-form-label">' + fhq.t('Text') + ' (Use markdown format)</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<textarea type="text" class="form-control" style="height: 150px" value="" id="edit_quest_text"></textarea>'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_score" class="col-sm-2 col-form-label">' + fhq.t('Score') + ' (+)</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<input type="number" class="form-control" id="edit_quest_score">'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_subject" class="col-sm-2 col-form-label">' + fhq.t('Subject') + '</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<select class="form-control" value="" id="edit_quest_subject">'
+		+ '						<option value="trivia">Trivia</option>'
+		+ '						<option value="hashes">Hashes</option>'
+		+ '						<option value="stego">Stego</option>'
+		+ '						<option value="reverse">Reverse</option>'
+		+ '						<option value="recon">Recon</option>'
+		+ '						<option value="crypto">Crypto</option>'
+		+ '						<option value="forensics">Forensics</option>'
+		+ '						<option value="network">Network</option>'
+		+ '						<option value="web">Web</option>'
+		+ '						<option value="ppc">PPC</option>'
+		+ '						<option value="admin">Admin</option>'
+		+ '						<option value="enjoy">Enjoy</option>'
+		+ '						<option value="unknown">Unknown</option>'
+		+ '					</select>'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_quest_answer" class="col-sm-2 col-form-label">' + fhq.t('Answer') + '</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<input type="text" class="form-control" id="edit_quest_answer" value="">'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_quest_answer_format" class="col-sm-2 col-form-label">' + fhq.t('Answer format') + '</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<input type="text" class="form-control" id="edit_quest_answer_format" value="">'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_author" class="col-sm-2 col-form-label">' + fhq.t('Author') + '</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<input type="text" class="form-control" value="" id="edit_quest_author">'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_copyright" class="col-sm-2 col-form-label">' + fhq.t('Copyright') + '</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<input type="text" class="form-control" value="" id="edit_quest_copyright">'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_quest_state" class="col-sm-2 col-form-label">' + fhq.t('State') + '</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<select class="form-control" value="" id="edit_quest_state">'
+		+ '						<option value="open">Open</option>'
+		+ '						<option value="closed">Closed</option>'
+		+ '						<option value="broken">Broken</option>'
+		+ '					</select>'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label for="edit_description_state" class="col-sm-2 col-form-label">' + fhq.t('Description State') + '</label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<textarea type="text" class="form-control" style="height: 150px" value="" id="edit_quest_description_state"></textarea>'
+		+ '				</div>'
+		+ '			</div>'
+		+ '			<div class="form-group row">'
+		+ '				<label class="col-sm-2 col-form-label"></label>'
+		+ ' 			<div class="col-sm-10">'
+		+ '					<div id="quest_update_error" class="alert alert-danger" style="display: none"></div>'
+		+ '				</div>'
+		+ '			</div>'
+		+ '		</div>'
+		+ '</div>'
+		+ '<div class="btn btn-danger" id="quest_update"><i class="fa fa-check"></i> Update</div> '
+		+ '<div class="btn btn-primary" id="quest_update_close"><i class="fa fa-times"></i> Cancel</div> '
+	);
+	window['edit_quest_text'] = new SimpleMDE({ element: $("#edit_quest_text")[0] });
+
+	$('#quest_update').unbind().bind('click', fhq.updateQuest);
+	$('#quest_update_close').unbind().bind('click', fhq.pages['quests']);
+
+	fhq.ws.games().done(function(r){
+		console.log(r);
+		for(var i in r.data) {
+			$('#edit_quest_gameid').append('<option value="' + r.data[i]["local_id"] + '">' + r.data[i]["name"] + '</option>');
+		}
+		fhq.ws.quest({"questid": questid}).done(function(data){
+			var q = data.quest;
+			var hints = data.hints;
+			var files = data.files;
+			// console.log(data);
+			for (var p in q) {
+				var edit_id = 'edit_quest_' + p;
+				console.log(edit_id)
+				if ($('#' + edit_id).length > 0) {
+					if (edit_id == "edit_quest_text") {
+						window['edit_quest_text'].value(q[p]);
+					} else {
+						$('#' + edit_id).val(q[p]);
+					}
+				} else {
+					console.warn("Not found field: " + edit_id);
+				}
+			}
+		}).fail(function(err){
+			console.error(err);
+		})
+
+	})
+}
+
+
+fhq.updateQuest = function() {
+	fhq.showLoader();
+	var params = {};
+	params["questid"] = $("#edit_quest_id").val();
+	params["questid"] = parseInt(params["questid"],10);
+	params["gameid"] = parseInt($("#edit_quest_gameid").val(),10);
+	params["name"] = $("#edit_quest_name").val();
+	params["text"] = window['edit_quest_text'].value();
+	params["score"] = parseInt($("#edit_quest_score").val(),10);
+	params["subject"] = $("#edit_quest_subject").val();
+	params["copyright"] = $("#edit_quest_copyright").val();
+	params["author"] = $("#edit_quest_author").val();
+	params["answer"] = $("#edit_quest_answer").val();
+	params["answer_format"] = $("#edit_quest_answer_format").val();
+	params["state"] = $("#edit_quest_state").val();
+	params["description_state"] = $("#edit_quest_description_state").val();
+
+	fhq.ws.quest_update(params).done(function(r){
+		fhq.hideLoader();
+		// fhq.loadQuest(r.questid);
+		fhq.pages['quests']();
+	}).fail(function(err){
+		fhq.hideLoader();
+		console.error(err);
+		$('#quest_update_error').show();
+		$('#quest_update_error').html(err.error);
 		
 	});
 };
