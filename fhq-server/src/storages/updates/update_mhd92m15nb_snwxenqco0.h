46cbbdf028b9bf92ac17f03630a0ba130f539e1d
atrest890 <swarovski991@gmail.com>
2019-10-26 12:41:31 +0700

    add database updates

---
+++ fhq-server/src/storages/updates/update_mhd92m15nb_snwxenqco0.h
@@ -0,0 +1,11 @@
+#ifndef UPDATE_MHD92M15NB_SNWXENQCO0_H
+#define UPDATE_MHD92M15NB_SNWXENQCO0_H
+
+#include <storages.h>
+
+class Update_mhd92m15nb_snwxenqco0 : public StorageUpdateBase {
+    public:
+        Update_mhd92m15nb_snwxenqco0();
+};
+
+#endif // UPDATE_MHD92M15NB_SNWXENQCO0_H
