6f9e963b5eaa63599dc6ddab70555a3049912079
atrest890 <swarovski991@gmail.com>
2019-11-23 11:02:37 +0700

    Add chat handlers definitions

--- fhq-server/src/cmd/cmd_handlers_communication.h
+++ fhq-server/src/cmd/cmd_handlers_communication.h
@@ -19,4 +19,65 @@ class CmdHandlerChatLastestMessages : public CmdHandlerBase {
         virtual void handle(ModelRequest *pRequest);
 };
 
+class CmdHandlerChatReadMessage : public CmdHandlerBase {
+    public:
+        CmdHandlerChatReadMessage();
+        virtual void handle(ModelRequest *pRequest);
+}
+
+class CmdHandlerChatShowDialogs : public CmdHandlerBase {
+    public:
+        CmdHandlerChatShowDialogs();
+        virtual void handle(ModelRequest *pRequest);
+}
+
+class CmdHandlerChatEditMessage : public CmdHandlerBase {
+    public:
+        CmdHandlerChatEditMessage();
+        virtual void handle(ModelRequest *pRequest);
+}
+
+class CmdHandlerChatDeleteMessage : public CmdHandlerBase {
+    public:
+        CmdHandlerChatDeleteMessage();
+        virtual void handle(ModelRequest *pRequest);
+}
+
+class CmdHandlerChatAddToChat : public CmdHandlerBase {
+    public:
+        CmdHandlerChatAddToChat();
+        virtual void handle(ModelRequest *pRequest);
+}
+
+class CmdHandlerChatDeleteFromChat : public CmdHandlerBase {
+    public:
+        CmdHandlerChatDeleteFromChat();
+        virtual void handle(ModelRequest *pRequest);
+}
+
+class CmdHandlerChatChangeOwner : public CmdHandlerBase {
+    public:
+        CmdHandlerChatChangeOwner();
+        virtual void handle(ModelRequest *pRequest);
+}
+
+class CmdHandlerChatAddToBlackList : public CmdHandlerBase {
+    public:
+        CmdHandlerChatAddToBlackList();
+        virtual void handle(ModelRequest *pRequest);
+}
+
+class CmdHandlerChatDeleteFromBlackList : public CmdHandlerBase {
+    public:
+        CmdHandlerChatShowDialogs();
+        virtual void handle(ModelRequest *pRequest);
+}
+
+class CmdHandlerChatCreateGroupChat : public CmdHandlerBase {
+    public:
+        CmdHandlerChatCreateGroupChat();
+        virtual void handle(ModelRequest *pRequest);
+}
+
+
 #endif // CMD_HANDLERS_COMMUNICATION_H
