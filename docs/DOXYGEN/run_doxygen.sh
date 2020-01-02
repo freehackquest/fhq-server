4464e95c5437073edfeab45ec19054fdbf704098
Evgenii Sopov <mrseakg@gmail.com>
2020-01-02 13:50:42 +0700

    Updated run_doxygen

--- docs/DOXYGEN/run_doxygen.sh
+++ docs/DOXYGEN/run_doxygen.sh
@@ -3,6 +3,9 @@
 FHQSRV_VERSION=$(cat ../../fhq-server/CMakeLists.txt | grep -Po '(?<=FHQSRV_VERSION=")\d+\.\d+\.\d+')
 echo $FHQSRV_VERSION
 
+rm doxygen_objdb_*.tmp
+rm doxygen_entrydb_*.tmp
+
 export FHQSRV_VERSION="$FHQSRV_VERSION" 
 
 doxygen
