0729e0bf2bace2109104cca50edb8e4fe2fe3687
Evgenii Sopov <mrseakg@gmail.com>
2020-11-26 09:02:05 +0700

    Fixed #18: drop column games.rules

---
+++ fhq-server/src/storages/updates/update_pmlyt8k7nm_215438peu8.h
@@ -0,0 +1,11 @@
+#ifndef UPDATE_PMLYT8K7NM_215438PEU8_H
+#define UPDATE_PMLYT8K7NM_215438PEU8_H
+
+#include <wsjcpp_storages.h>
+
+class Update_pmlyt8k7nm_215438peu8 : public WsjcppStorageUpdateBase {
+    public:
+        Update_pmlyt8k7nm_215438peu8();
+};
+
+#endif // UPDATE_PMLYT8K7NM_215438PEU8_H
