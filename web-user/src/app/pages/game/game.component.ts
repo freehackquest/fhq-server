a2deae0cb6d327707f62f715944db12e2a861c5a
Evgenii Sopov <mrseakg@gmail.com>
2021-03-28 01:54:49 +0700

    Start page for game info

--- web-user/src/app/pages/game/game.component.ts
+++ web-user/src/app/pages/game/game.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { SpinnerService } from '../../services/spinner.service';
+import { FhqService } from 'src/app/services/fhq.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  gameuuid: string;
+  errorMessage: string = null;
+  subscription: any = null;
+
+  constructor(
+    private _spinner: SpinnerService,
+    private _cdr: ChangeDetectorRef,
+    public _fhq: FhqService,
+    private _route: ActivatedRoute,
+    private _router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this._route.params.subscribe( (params) => {
+      if (!params['id']) {
+        this._router.navigate(['/games']);
+        return;
+      }
+      this.gameuuid = params['uuid'];
+      // TODO check possible subjects
+      this.loadData();
+
+      this.subscription = this._fhq.changedState
+        .subscribe(() => this.loadData());
+    });
   }
 
+  loadData() {
+    const _data = {
+      uuid: this.gameuuid
+    }
+    this._spinner.show();
+    this._fhq.api().game_info(_data)
+      .done((r: any) => this.successResponse(r))
+      .fail((err: any) => this.errorResponse(err));
+  }
+
+  successResponse(r: any) {
+    console.log(r);
+    this._spinner.hide();
+    this._cdr.detectChanges();
+  }
+
+  errorResponse(err: any) {
+    this._spinner.hide();
+    this.errorMessage = err.error;
+    this._cdr.detectChanges();
+    console.error(err);
+  }
 }
