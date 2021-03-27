4c6237e3b69ce59c51fa2cf091b98954ea5c6f0b
Evgenii Sopov <mrseakg@gmail.com>
2021-03-28 01:42:05 +0700

    Redesign sign dialog to material

--- web-user/src/app/dialogs/modal-signin-dialog/modal-signin-dialog.component.ts
+++ web-user/src/app/dialogs/modal-signin-dialog/modal-signin-dialog.component.ts
@@ -1,24 +1,32 @@
-import { Component, OnInit, Input, ChangeDetectorRef, NgZone, ViewChild, ElementRef } from '@angular/core';
+import { Component, Inject, OnInit, Input, ChangeDetectorRef, NgZone, ViewChild, ElementRef } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { SpinnerService } from '../../services/spinner.service';
 import { EmailValidatorService } from '../../services/email-validator.service';
-import { Location } from '@angular/common';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 declare var fhq: any;
 
+export interface DialogData {
+  email: string;
+  password: string;
+}
+
 @Component({
-  selector: 'app-modal-dialog-sign-in',
-  templateUrl: './modal-dialog-sign-in.component.html',
-  styleUrls: ['./modal-dialog-sign-in.component.css']
+  selector: 'app-modal-signin-dialog',
+  templateUrl: './modal-signin-dialog.component.html',
+  styleUrls: ['./modal-signin-dialog.component.css']
 })
-export class ModalDialogSignInComponent implements OnInit {
+export class ModalSignInDialog implements OnInit {
   errorMessage: string = null;
   name: string = '';
   @ViewChild('signinEmail', { static: true }) signinEmail : ElementRef;
   @ViewChild('signinPassword', { static: true }) signinPassword : ElementRef;
 
+  email: string;
+  password: string;
+  width: string = "450px";
   constructor(
     public _activeModal: NgbActiveModal,
     private _route: ActivatedRoute,
@@ -27,20 +35,53 @@ export class ModalDialogSignInComponent implements OnInit {
     private _zone: NgZone,
     private _spinnerService: SpinnerService,
     private _emailValidator: EmailValidatorService,
+    public dialog: MatDialog,
+    public dialogRef: MatDialogRef<ModalSignInDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
   ) { }
 
   ngOnInit() {
 
   }
 
+  // openDialog(): void {
+  //   const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+  //     width: '250px',
+  //     data: {name: this.name, animal: this.animal}
+  //   });
+  // 
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     console.log('The dialog was closed');
+  //     this.animal = result;
+  //   });
+  // }
+
+  onCloseClick(): void {
+    this.dialogRef.close();
+  }
+
+  onSignInClick(): void {
+    console.log("SignIn");
+    this._spinnerService.show();
+    this.errorMessage = null;
+    fhq.login({
+      "email": this.data.email,
+      "password": this.data.password,
+    })
+      .done((r: any) => this.successResponse(r))
+      .fail((err: any) => this.errorResponse(err));
+  }
+  
   openResetPasswordPage() {
     this._activeModal.dismiss('Reset password');
     this._zone.run(() => this._router.navigate(['/reset-password'])).then();
+    this.dialogRef.close();
   }
 
   openResgistrationPage() {
     this._activeModal.dismiss('Registration');
     this._zone.run(() => this._router.navigate(['/registration'])).then();
+    this.dialogRef.close();
   }
 
   signInGo() {
@@ -61,6 +102,8 @@ export class ModalDialogSignInComponent implements OnInit {
     this._cdr.detectChanges();
     this._spinnerService.hide();
     this._activeModal.dismiss('SignIn Success');
+    this.dialogRef.close();
+
     // location.reload();
   }
 
