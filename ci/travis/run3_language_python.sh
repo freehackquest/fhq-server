a997f30f39f0700c3ef1f65aac4d2ae7d629b93d
Evgenii Sopov <mrseakg@gmail.com>
2021-09-14 13:34:54 +0700

    Reconfigure .travis.yml to jobs

---
+++ ci/travis/run3_language_python.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+check_ret() {
+    if [ $1 -ne 0 ]; then
+        echo ""
+        echo "!!! FAIL: $2"
+        echo "********************************************************************************"
+        echo ""
+        exit $1
+    else
+        echo ""
+        echo "*** SUCCESS: $2"
+        echo "********************************************************************************"
+        echo ""
+    fi
+} 
+
+check_ret $? "change directory to fhq-server"
+python3 -m sea5kg_cpplint
+check_ret $? "sea5kg_cpplint"
+
+cd fhq-server-tests
+check_ret $? "Change directory to tests"
+./update_freehackquest_libclient_py.sh
+check_ret $? "Update freehackquest_libclient_py python library"
+python3 run_tests.py # run tests
+check_ret $? "tests"
+cd  ..
+
+cd tests/server-api-tests
+check_ret $? "Change directory to tests/server-api-tests"
+./update_freehackquest_libclient_py.sh
+check_ret $? "Update freehackquest_libclient_py python library"
+python3 -m pylint --version
+# in next time replace to 'pylint **/*.py'
+python3 -m pylint --rcfile=.pylintrc *.py
+check_ret $? "Check by pylint tests_leaks.py"
+python3 -m pytest -rAs -c env-travis.ini
+check_ret $? "Run tests_leaks.py"
+cd ../..
