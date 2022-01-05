8e0b70d884dcba32ae1d7fe05836471ae4ccee08
Evgenii Sopov <mrseakg@gmail.com>
2022-01-05 15:29:19 +0700

    Implemented downloads counter for files

--- src/employees/employ_files.cpp
+++ src/employees/employ_files.cpp
@@ -19,7 +19,7 @@ EmployFiles::EmployFiles()
 // ---------------------------------------------------------------------
 
 bool EmployFiles::init() {
-    // TODO mutex
+    std::lock_guard<std::mutex> lock(m_mtxFiles);
     // check the access to games folder
     EmployGlobalSettings *pGlobalSettings = findWsjcppEmploy<EmployGlobalSettings>();
     std::string sPublicFolder = pGlobalSettings->get("web_public_folder").getDirPathValue();
@@ -43,6 +43,7 @@ bool EmployFiles::init() {
     EmployDatabase *pDatabase = findWsjcppEmploy<EmployDatabase>();
     QSqlDatabase db = *(pDatabase->database());
     m_nSizeOfFiles = 0;
+    m_nAllDownloadsCounter = 0;
     QSqlQuery query(db);
     query.prepare("SELECT * FROM quests_files");
 
@@ -58,6 +59,9 @@ bool EmployFiles::init() {
         std::string sUuid = record.value("uuid").toString().toStdString();
         pModelQuestFile->setUuid(sUuid);
         pModelQuestFile->setQuestLocalId(record.value("questid").toInt());
+        int nDownloadsCounter = record.value("downloads_counter").toInt();
+        pModelQuestFile->setDownloadsCounter(nDownloadsCounter);
+        m_nAllDownloadsCounter += nDownloadsCounter;
         pModelQuestFile->setFileName(record.value("filename").toString().toStdString());
         int nFileSize = record.value("size").toString().toInt();
         // TODO check max size
@@ -67,6 +71,9 @@ bool EmployFiles::init() {
         std::string sFilePath = record.value("filepath").toString().toStdString();
         sFilePath = WsjcppCore::doNormalizePath("/" + sFilePath);
         pModelQuestFile->setFilePath(sFilePath);
+        pModelQuestFile->setMd5(record.value("md5").toString().toStdString());
+        // TODO calculate md5 if is empty and update in database
+
         // TODO must be cached all fields
 
         if (m_mapCacheQuestFilesByUuid.count(sUuid)) {
@@ -108,7 +115,7 @@ int EmployFiles::getSizeOfFiles() {
 }
 
 bool EmployFiles::findQuestFile(int nLocalId, ModelQuestFile &modelGame) {
-    // TODO mutex
+    std::lock_guard<std::mutex> lock(m_mtxFiles);
     for (int i = 0; i < m_vCacheQuestFiles.size(); i++) { // TODO create map with index
         if (m_vCacheQuestFiles[i]->getLocalId() == nLocalId) {
             modelGame.copy(*m_vCacheQuestFiles[i]);
@@ -119,10 +126,7 @@ bool EmployFiles::findQuestFile(int nLocalId, ModelQuestFile &modelGame) {
 }
 
 bool EmployFiles::findQuestFileByFilePath(const std::string &sFilePath, ModelQuestFile &m) {
-    
-    std::cout << "m_mapCacheQuestFilesByFilePath.size(): " << m_mapCacheQuestFilesByFilePath.size() << std::endl;
-    std::cout << "m_vCacheQuestFiles.size(): " << m_vCacheQuestFiles.size() << std::endl;
-
+    std::lock_guard<std::mutex> lock(m_mtxFiles);
     if (!m_mapCacheQuestFilesByFilePath.count(sFilePath)) {
         return false;
     }
@@ -131,9 +135,44 @@ bool EmployFiles::findQuestFileByFilePath(const std::string &sFilePath, ModelQue
 }
 
 bool EmployFiles::findQuestFileByUuid(const std::string &sUuid, ModelQuestFile &m) {
+    std::lock_guard<std::mutex> lock(m_mtxFiles);
     if (!m_mapCacheQuestFilesByUuid.count(sUuid)) {
         return false;
     }
     m.copy(*m_mapCacheQuestFilesByUuid.at(sUuid));
     return true;
+}
+
+std::vector<ModelQuestFile*> EmployFiles::findFilesByQuestId(int nQuestId) {
+    std::lock_guard<std::mutex> lock(m_mtxFiles);
+    std::vector<ModelQuestFile*> vRet;
+    for (int i = 0; i < m_vCacheQuestFiles.size(); i++) { // TODO create map with index
+        if (m_vCacheQuestFiles[i]->getQuestLocalId() == nQuestId) {
+            vRet.push_back(m_vCacheQuestFiles[i]);
+        }
+    }
+    return vRet;
+}
+
+void EmployFiles::updateDownloadsCounter(const ModelQuestFile &m) {
+    std::lock_guard<std::mutex> lock(m_mtxFiles);
+    if (m_mapCacheQuestFilesByUuid.count(m.getUuid())) {
+        ModelQuestFile* pModel = m_mapCacheQuestFilesByUuid.at(m.getUuid());
+        int nNewVal = pModel->getDownloadsCounter() + 1;
+        m_nAllDownloadsCounter++;
+        pModel->setDownloadsCounter(nNewVal);
+
+        EmployDatabase *pDatabase = findWsjcppEmploy<EmployDatabase>();
+        QSqlDatabase db = *(pDatabase->database());
+        {
+            QSqlQuery query(db);
+            query.prepare("UPDATE quests_files SET downloads_counter = :downloads_counter WHERE uuid = :uuid");
+            query.bindValue(":downloads_counter", nNewVal);
+            query.bindValue(":uuid", QString::fromStdString(pModel->getUuid()));
+            if (!query.exec()) {
+                WsjcppLog::err(TAG, query.lastError().text().toStdString());
+                return;
+            }
+        }
+    }
 }
