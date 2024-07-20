c44ccb5d62a2b178a04c9635eeaa60778170566a
Evgenii Sopov <mrseakg@gmail.com>
2024-07-20 15:05:11 +0700

    Fix build

--- build_simple.sh
+++ build_simple.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-if [ ! -d tmp ]; then
-	mkdir -p tmp
+check_ret() {
+    if [ $1 -ne 0 ]; then
+        echo ""
+        echo "!!! FAIL: $2"
+        echo "********************************************************************************"
+        echo ""
+        exit $1
+    else
+        echo ""
+        echo "*** SUCCESS: $2"
+        echo "********************************************************************************"
+        echo ""
+    fi
+}
+
+if [ ! -d ./tmp/release ]; then
+	mkdir -p ./tmp/release
 fi
 
-cd tmp
-cmake ..
-make
+cmake -H. -B./tmp/release -DCMAKE_BUILD_TYPE=Release
+check_ret $? "configure"
+
+cmake --build ./tmp/release --config Release
+check_ret $? "build"
