24a9dcfaf8d27dfe9034fd3b4cee9711c6bfe635
Evgenii Sopov <mrseakg@gmail.com>
2024-08-05 09:35:39 +0700

    Moved clean.sh to './pm.py clean'

--- libfhqpm/__init__.py
+++ libfhqpm/__init__.py
@@ -2,7 +2,11 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2020-2024 FreeHackQuest Team <freehackquest@gmail.com>
 
+""" init module fro libfhqpm (Project Manager) helper lib for a proccesses project """
+
 from .commands_helper import CommandsHelper
 from .create_storage_update import CreateStorageUpdate
 from .code_check import CodeCheck
 from .rebuild_environment_images import RebuildEnvironmentImages
+from .subcommand_clean import SubcommandClean
+from .subcommands import Subcommands
---
+++ libfhqpm/subcommand_clean.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+##################################################################################
+#    __ _
+#   / _| |__   __ _       ___  ___ _ ____   _____ _ __
+#  | |_| '_ \ / _` | ___ / __|/ _ \ '__\ \ / / _ \ '__|
+#  |  _| | | | (_| ||___|\__ \  __/ |   \ V /  __/ |
+#  |_| |_| |_|\__, |     |___/\___|_|    \_/ \___|_|
+#                |_|
+#
+# Copyright (c) 2011-2024 FreeHackQuest <freehackquest@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+##################################################################################
+
+import os
+
+class SubcommandClean:
+    def __init__(self):
+        self.__subcomamnd_name = "clean"
+
+    def get_name(self):
+        """ return subcommand name """
+        return self.__subcomamnd_name
+
+    def do_registry(self, subparsers):
+        """ registring sub command """
+        _parser = subparsers.add_parser(
+            name=self.__subcomamnd_name,
+            description='Clean'
+        )
+        _parser.set_defaults(subparser=self.__subcomamnd_name)
+
+    def execute(self):
+        print("Cleaning...")
+
+        to_remove = [
+            "rm -f Makefile",
+            "rm -rf build",
+            "rm -rf tmp",
+            "rm -f fhq-server",
+            "rm -f .qmake.stash",
+            "rm -f fhq-server.pro.user",
+            "rm -f CMakeLists.txt.user",
+            "rm -f debian/files",
+        ]
+        for _cmd in to_remove:
+            print("Run '" + _cmd + "'")
+            os.system(_cmd)
