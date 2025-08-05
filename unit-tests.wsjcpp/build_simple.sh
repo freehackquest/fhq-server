4b9b8e33147d2c0f891fed36b543ac312004ea0c
Evgenii Sopov <mrseakg@gmail.com>
2025-08-05 10:23:43 +0700

    Up version to 0.2.53

--- unit-tests.wsjcpp/build_simple.sh
+++ unit-tests.wsjcpp/build_simple.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+check_ret() {
+    if [ $1 -ne 0 ]; then
+        echo ""
+        echo "!!! FAIL: $2"
+        echo "********************************************************************************"
+        echo ""
+        exit $1
+    else
+        echo ""
+        echo "*** SUCCESS: $2"
+        echo "********************************************************************************"
+        echo ""
+    fi
+}
+
 if [ ! -d tmp ]; then
     mkdir -p tmp
 fi
 
 cd tmp
 cmake ..
+check_ret $? "configure"
+
 make
+check_ret $? "make"
 
