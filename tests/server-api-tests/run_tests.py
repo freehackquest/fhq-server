efdc4ba959db5c5678bbbefdeb08068c3863ff8e
Evgenii Sopov <mrseakg@gmail.com>
2026-04-18 12:15:45 +0700

    tests/server-api-tests: removed 'import glob2'

--- tests/server-api-tests/run_tests.py
+++ tests/server-api-tests/run_tests.py
@@ -4,8 +4,26 @@
 """ run all tests """
 
 import sys
+import os
 import subprocess
-import glob2
+
+def recursive_find_all_py_files(_dir, ignores=None):
+    """ recursive_find_all_cbproj_files """
+    _ret = []
+    for _file in os.listdir(_dir):
+        _fullpath = os.path.join(_dir, _file)
+        if _file == '.git':
+            continue
+        if not _file.endswith(".py"):
+            continue
+        if ignores is not None and _file in ignores:
+            continue
+        if os.path.isdir(_fullpath):
+            _ret.extend(recursive_find_all_py_files(_fullpath, ignores))
+        else:
+            _ret.append(_fullpath)
+    return _ret
+
 
 def run_command(_command, _output):
     """ run_command """
@@ -41,14 +59,25 @@ def run_command(_command, _output):
     sys.exit("Could not start process")
 
 if __name__ == "__main__":
-    with open('result_tests.txt', 'w') as _output:
+    with open('result_tests.txt', 'w', encoding="utf-8") as _output:
+        py_files = recursive_find_all_py_files(".", ignores=[
+            "__pycache__",
+            ".pytest_cache",
+            "freehackquest_libclient_py",
+        ])
         try:
             command = [
                 'python3', '-m', 'pylint',
                 '--rcfile=.pylintrc',
             ]
-            command.extend(glob2.glob("*.py"))
+            command.extend(py_files)
             run_command(command, _output)
+
+            command = [
+                'python3', '-m', 'pycodestyle',
+            ]
+            command.extend(py_files)
+
             run_command([
                 'python3', '-m', 'pytest',
                 '-rAs', '-c', 'env-travis.ini',
